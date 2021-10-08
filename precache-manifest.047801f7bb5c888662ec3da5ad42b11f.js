7ee017ed6cbab5290bc234d27b461496e2d7ff3b
Victor Shevchuk <callmevitok@gmail.com>
2021-10-08 13:47:10 +0300

    Updates

---
+++ precache-manifest.047801f7bb5c888662ec3da5ad42b11f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2590de3fa31d392ebbda7fa9161245f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "934cf2ab028da24469e5",
+    "url": "./static/css/main.5c421051.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "934cf2ab028da24469e5",
+    "url": "./static/js/main.05dec3e4.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
